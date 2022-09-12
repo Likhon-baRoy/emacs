6d2c906b6ace42bd51c56c70f8dd8b5a654af5fd
Likhon-baRoy <61086375+Likhon-baRoy@users.noreply.github.com>
2022-09-12 14:57:19 +0600

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,9 +3,7 @@
  '(mode-line-format
    '("%I %e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
      ((:eval
-	;; (number-to-string
-	;; 		(line-number-at-pos
-	;;          (point-max)))
+;;	   (number-to-string (line-number-at-pos (point-max)))
        (number-to-string
         (count-lines
          (point-min)
